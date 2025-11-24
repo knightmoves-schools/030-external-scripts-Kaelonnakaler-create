3e375b83f0a055dcd9a9ace10781fb7a22f2f0a4
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-11-24 20:16:22 +0000

    var total 50

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var total = 1;
-        total = total + 2;
-        total = total + 3;
-        total = total + 4;
-        total = total + 5;
+var total = 50;
+        
 
         document.getElementById('result').innerHTML = total;
